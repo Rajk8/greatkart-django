00c4ac07006d47c75aa22ebf34fdbeaac1194132
Rajesh <rajesh@mera.com>
2023-09-09 12:54:07 +0530

    This is my first greatkart

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'category',
     'accounts',
+    'store',
 ]
 
 MIDDLEWARE = [
@@ -64,6 +65,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'category.context_processors.menu_links',
             ],
         },
     },
@@ -129,3 +131,4 @@ STATICFILES_DIRS = [
 # media files configurations
 
 MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR /'media'
